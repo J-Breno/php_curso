d6775586e3ce7a5c9e982d85d6053de3f82a3d01
J-Breno <joao.breno85@hotmail.com>
2025-08-26 17:55:08 -0300

    Parâmetros - Referência ou valor

--- 01-funcoes-parametros-includes/index.php
+++ 01-funcoes-parametros-includes/index.php
@@ -1,9 +1,11 @@
 <?php
-function somar( int $n1, int $n2, int $n3 = 0 ): int {
-    return $n1 + $n2 + $n3;
+function somar( int $n1, int $n2, &$total): int {
+    $total = $n1 + $n2;
 }
 
-$x = somar(1, 3);
-$y = somar(5, 3, 2);
-$soma = somar($x, $y);
+$x = 3;
+$y = 2;
+$soma = 0;
+somar($x, $y, $soma);
+
 echo "TOTAL: ".$soma;
