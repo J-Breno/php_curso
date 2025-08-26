53d6e3c1a04224d6319cff713fec4d5cb22b48b0
J-Breno <joao.breno85@hotmail.com>
2025-08-26 17:49:04 -0300

    Parâmetros e Valor padrão

--- 01-funcoes-parametros-includes/index.php
+++ 01-funcoes-parametros-includes/index.php
@@ -1,9 +1,9 @@
 <?php
-function somar( $n1, $n2 ) {
-    return $n1 + $n2;
+function somar( int $n1, int $n2, int $n3 = 0 ): int {
+    return $n1 + $n2 + $n3;
 }
 
 $x = somar(1, 3);
-$y = somar(5, 3);
+$y = somar(5, 3, 2);
 $soma = somar($x, $y);
 echo "TOTAL: ".$soma;
