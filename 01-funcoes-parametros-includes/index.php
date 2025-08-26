680ce93c733e6621989a9234a379eb0fa235ae57
J-Breno <joao.breno85@hotmail.com>
2025-08-26 18:07:28 -0300

    funções nativas de matemática

--- 01-funcoes-parametros-includes/index.php
+++ 01-funcoes-parametros-includes/index.php
@@ -1,4 +1,13 @@
 <?php
-$dizimo = fn($valor) => $valor * 0.10;
+$numero = -8.4;
 
-echo $dizimo(10);
+echo abs($numero); // valor absoluto
+echo pi(); // pi
+echo floor($numero); // arredonda para baixo
+echo ceil($numero); // arredonda para cima
+echo round($numero, 2); //arredonda mais proximo e segundo parÂmetro é a precisão
+$aleatorio = rand(3, 9);
+echo $aleatorio;
+$lista = [1, 2, 3, 4, 5];
+echo max($lista);
+echo min($lista);
