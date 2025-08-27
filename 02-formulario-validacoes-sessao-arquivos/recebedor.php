f54fa2a544dc35862c3d82de20e456b33606a716
J-Breno <joao.breno85@hotmail.com>
2025-08-27 03:05:50 -0300

    Upload de arquivos parte2

--- 02-formulario-validacoes-sessao-arquivos/recebedor.php
+++ 02-formulario-validacoes-sessao-arquivos/recebedor.php
@@ -2,5 +2,12 @@
 echo "<pre>";
 print_r($_FILES);
 
-$nome = $_FILES['arquivo']['name'];
-move_uploaded_file( $_FILES['arquivo']['tmp_name'], 'arquivos/'.$nome   );
+$permitidos = ['image/jpeg', 'image/jpg', 'image/png'];
+
+if(in_array($_FILES['arquivo']['type'], $permitidos)) {
+    $nome = md5(time().rand(0, 1000)).'.jpg';
+    move_uploaded_file( $_FILES['arquivo']['tmp_name'], 'arquivos/'.$nome);
+    echo 'Arquivo salvo com sucesso';
+} else {
+    echo 'Arquivo não permitido';
+}
