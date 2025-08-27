af566302ba6a855ab2117bad15ec196c627694d3
J-Breno <joao.breno85@hotmail.com>
2025-08-27 05:12:21 -0300

    Entendendo interface

--- 04-orientacao-a-objetos/index.php
+++ 04-orientacao-a-objetos/index.php
@@ -1,48 +1,24 @@
 <?php
-class Post {
-    private int $id;
-    private int $likes = 0;
-
-    public function setId(int $id)
-    {
-        $this->id = $id;
-    }
-
-    public function  getId()
-    {
-        return $this->id;
-    }
-
-    public function setLikes(int $likes)
-    {
-        $this->likes = $likes;
-    }
-
-    public function  getLikes()
-    {
-        return $this->likes;
-    }
+interface Database {
+    public function listarProdutos();
+    public function adicionarProduto();
+    public function alterarProduto();
 }
 
-class Foto extends Post {
-    private $url;
-
-    public function _construct($id) {
-        $this->setId($id);
-    }
-
-    public function setUrl($u)
+class MysqlDB implements Database {
+    public function listarProdutos()
     {
-        $this->url = $u;
+
     }
 
-    public function getUrl()
+    public function adicionarProduto()
     {
-        return $this->url;
+        echo "Adicionando com MySql";
     }
+
+    public function alterarProduto()
+    {
+        
+    }
+
 }
-
-$foto = new Foto(20);
-$foto->setLikes(12);
-
-echo "FOTO: #".$foto->getId().' - '.$foto->getLikes()." likes";
