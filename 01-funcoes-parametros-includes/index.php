0e60f999bacdd178d93e5f701017d6950a480a9a
J-Breno <joao.breno85@hotmail.com>
2025-08-27 00:30:41 -0300

    funções nativas de Data - Hora

--- 01-funcoes-parametros-includes/index.php
+++ 01-funcoes-parametros-includes/index.php
@@ -1,38 +1,6 @@
 <?php
-$lista = ['nome1', 'nome2', 'nome3', 'nome4', 'nome5'];
-$aprovados = ['nome1', 'nome2', 'nome3'];
-$reprovados = array_diff($lista, $aprovados);
-
-echo "Total;: ".count($lista);
-print_r($reprovados);
-
-$numeros = [10, 20, 30, 40, 50];
-$filtrados = array_filter($numeros, function ($item) {
-    if($item < 30) {
-        return true;
-    } else {
-        return false;
-    }
-});
-
-print_r($filtrados);
-$dobrados = array_map(function ($item) {
-    return $item * 2;
-}, $numeros);
-print_r($dobrados);
-
-array_pop($numeros);
-array_shift($numeros);
-
-if(in_array(90, $numeros)) {
-    return 'Existe';
-} else {
-    return 'Não existe.';
-}
-
-$pos = array_search($numeros);
-echo sort($numeros);
-echo rsort($numeros );
-asort($numeros);
-
-$nome3 = implode('@', $nmmmeros);''
+echo date('d/m/Y H:i:s');
+$data = '2025-08-27';
+$time = strtotime($data);
+echo date('d/m/Y', $time);
+echo date('d/m/Y', strtotime($data));
