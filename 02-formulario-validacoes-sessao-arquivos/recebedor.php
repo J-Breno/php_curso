ec3d20b2161e9f35628ca3ff078c71e808689d82
J-Breno <joao.breno85@hotmail.com>
2025-08-27 01:07:11 -0300

    Pegando informações do formulário

--- 02-formulario-validacoes-sessao-arquivos/recebedor.php
+++ 02-formulario-validacoes-sessao-arquivos/recebedor.php
@@ -1,1 +1,11 @@
 <?php
+$nome = filter_input(INPUT_POST, 'nome');
+$idade = filter_input(INPUT_POST, 'idade');
+
+if($nome && $idade) {
+    echo 'Nome: '.$nome."<br />";
+    echo 'Idade: '.$idade;
+} else {
+    header('Location: index.php');
+    exit;
+}
