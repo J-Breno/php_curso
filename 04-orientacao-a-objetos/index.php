3e928bc124e7735a361407796fb91e271455268b
J-Breno <joao.breno85@hotmail.com>
2025-08-27 04:31:16 -0300

    Entendendo sobre herança

--- 04-orientacao-a-objetos/index.php
+++ 04-orientacao-a-objetos/index.php
@@ -1,9 +1,48 @@
 <?php
-class Matematica {
-    public static function somar(int $x,int $y): int
+class Post {
+    private int $id;
+    private int $likes = 0;
+
+    public function setId(int $id)
     {
-        return $x + $y;
+        $this->id = $id;
+    }
+
+    public function  getId()
+    {
+        return $this->id;
+    }
+
+    public function setLikes(int $likes)
+    {
+        $this->likes = $likes;
+    }
+
+    public function  getLikes()
+    {
+        return $this->likes;
     }
 }
 
-Matematica::somar(3, 4)
+class Foto extends Post {
+    private $url;
+
+    public function _construct($id) {
+        $this->setId($id);
+    }
+
+    public function setUrl($u)
+    {
+        $this->url = $u;
+    }
+
+    public function getUrl()
+    {
+        return $this->url;
+    }
+}
+
+$foto = new Foto(20);
+$foto->setLikes(12);
+
+echo "FOTO: #".$foto->getId().' - '.$foto->getLikes()." likes";
