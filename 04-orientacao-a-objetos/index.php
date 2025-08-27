47b10e5059398e6464c59807e6165e1c241164cd
J-Breno <joao.breno85@hotmail.com>
2025-08-27 04:22:00 -0300

    Método estático

--- 04-orientacao-a-objetos/index.php
+++ 04-orientacao-a-objetos/index.php
@@ -1,38 +1,9 @@
 <?php
-class Post {
-    public int $id;
-    public int $likes = 0;
-    public array $comments = [];
-    private string $author;
-
-    public function __construct(int $qtdLikes) {
-        $this->likes = $qtdLikes;
-    }
-
-    public function aumentarLike() {
-        $this->likes++;
-    }
-
-    public function setAuthor($n)
+class Matematica {
+    public static function somar(int $x,int $y): int
     {
-        if(strlen($n) >= 3) {
-            $this->author = ucfirst($n);
-        }
-    }
-
-    public function  getAuthor(): string
-    {
-        return $this->author ?? 'Visitante';
+        return $x + $y;
     }
 }
 
-$post1 = new Post(25);
-$post1->aumentarLike();
-$post1->aumentarLike();
-$post1->setAuthor("Breno");
-
-$post2 = new Post(2);
-$post2->aumentarLike();
-$post2->setAuthor("João");
-echo 'POST 1: '.$post1->likes." likes - ".$post1->getAuthor()."<br />";
-echo 'POST 2: '.$post2->likes." likes - ".$post2->getAuthor()."<br />";
+Matematica::somar(3, 4)
