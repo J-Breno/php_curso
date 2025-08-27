17561e0927e2e8be1f3b52feba374ea8f7e4925d
J-Breno <joao.breno85@hotmail.com>
2025-08-26 23:22:23 -0300

    funções nativas de String parte2

--- 01-funcoes-parametros-includes/index.php
+++ 01-funcoes-parametros-includes/index.php
@@ -6,7 +6,7 @@ echo "NOME SUJO: ".strlen($nomeSujo)."<br/>";
 echo "NOME LIMPO: ".strlen($nomeLimpo);
 
 $nome = "Breno";
-echo strtolower($nome);
+echo ucfirst(strtolower($nome));
 echo strtoupper($nome);
 
 $nomeAlterado = str_replace("Breno", "João", $nome);
@@ -16,3 +16,12 @@ $nomeCompleto = "João Breno";
 
 $nome2 = substr($nomeCompleto, 3, 3);
 echo $nome2;
+
+$posicao = strpos($nomeCompleto, 'a');
+echo $posicao;
+
+$nomes = explode(' ', $nomeCompleto);
+print_r($nomes);
+
+$numero = 12913.12;
+echo number_format($numero, 2, ',', '.');
