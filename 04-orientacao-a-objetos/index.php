3015c99104639466ed3bc9cec7e732c312566d37
J-Breno <joao.breno85@hotmail.com>
2025-08-27 04:18:25 -0300

    Entendendo encapsulamento

--- 04-orientacao-a-objetos/index.php
+++ 04-orientacao-a-objetos/index.php
@@ -3,7 +3,7 @@ class Post {
     public int $id;
     public int $likes = 0;
     public array $comments = [];
-    public string $author;
+    private string $author;
 
     public function __construct(int $qtdLikes) {
         $this->likes = $qtdLikes;
@@ -13,15 +13,26 @@ class Post {
         $this->likes++;
     }
 
+    public function setAuthor($n)
+    {
+        if(strlen($n) >= 3) {
+            $this->author = ucfirst($n);
+        }
+    }
 
+    public function  getAuthor(): string
+    {
+        return $this->author ?? 'Visitante';
+    }
 }
 
 $post1 = new Post(25);
 $post1->aumentarLike();
 $post1->aumentarLike();
+$post1->setAuthor("Breno");
 
 $post2 = new Post(2);
 $post2->aumentarLike();
-
-echo 'POST 1: '.$post1->likes."<br />";
-echo 'POST 2: '.$post2->likes."<br />";
+$post2->setAuthor("João");
+echo 'POST 1: '.$post1->likes." likes - ".$post1->getAuthor()."<br />";
+echo 'POST 2: '.$post2->likes." likes - ".$post2->getAuthor()."<br />";
