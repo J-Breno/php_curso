b428d27c22829982257796c6ff25539d47f21a9c
J-Breno <joao.breno85@hotmail.com>
2025-08-28 09:51:08 -0300

    Read - Lendo dados com PDO

--- 05-banco-de-dados-crud-dao-solid/index.php
+++ 05-banco-de-dados-crud-dao-solid/index.php
@@ -1,5 +1,12 @@
 <?php
 require 'config.php';
+
+$lista = [];
+$sql = $pdo->query("SELECT * FROM usuarios");
+if($sql->rowCount() > 0) {
+    $lista = $sql->fetchAll(PDO::FETCH_ASSOC);
+}
+
 ?>
 <a href="adicionar.php">ADICIONAR USUÁRIO</a>
 
@@ -10,4 +17,15 @@ require 'config.php';
         <th>EMAIL</th>
         <th>AÇÕES</th>
     </tr>
+    <?php foreach ($lista as $usuario): ?>
+        <tr>
+            <td><?php echo $usuario['id'];?></td>
+            <td><?=$usuario['nome'];?></td>
+            <td><?=$usuario['email'];?></td>
+            <td>
+                <a href="editar.php?id=<?=$usuario['id'];?>">[ Editar ]</a>
+                <a href="excluir.php?id=<?=$usuario['id'];?>">[ Excluir ]</a>
+            </td>
+        </tr>
+    <?php endforeach; ?>
 </table>
