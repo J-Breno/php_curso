d9b45eb678688c6e47a0a1c83a51cd144b9581d6
J-Breno <joao.breno85@hotmail.com>
2025-08-28 09:02:15 -0300

    Conectando ao banco de dados com PDO

---
+++ 05-banco-de-dados-crud-dao-solid/index.php
@@ -0,0 +1,10 @@
+<?php
+$pdo = new PDO("mysql:dbname=test;host=localhost", "root", "123456");
+
+$sql = $pdo->query('SELECT * FROM usuarios');
+
+echo "TOTAL: ".$sql->rowCount();
+
+$dados = $sql->fetchAll(PDO::FETCH_ASSOC);
+echo '<pre>';
+print_r($dados);
