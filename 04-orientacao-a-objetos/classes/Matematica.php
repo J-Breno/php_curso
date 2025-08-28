ad2c825839bba2a4650d804dc4f1acced3d3fd80
J-Breno <joao.breno85@hotmail.com>
2025-08-27 21:50:00 -0300

    Entendendo autoload

---
+++ 04-orientacao-a-objetos/classes/Matematica.php
@@ -0,0 +1,16 @@
+<?php
+class Matematica {
+    const VERSION = '1.0';
+
+    private $primeiroNumero;
+
+    public function somar(int $x, int $y): int
+    {
+        return $x + $y;
+    }
+
+    public function listarTudo()
+    {
+
+    }
+}
