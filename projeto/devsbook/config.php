2395e5e33a7b660ceb80b572cf929972ac494762
J-Breno <joao.breno85@hotmail.com>
2025-08-30 00:02:20 -0300

    Organizando pastas, iniciando projeto

---
+++ projeto/devsbook/config.php
@@ -0,0 +1,8 @@
+<?php
+$base = "http://localhost/devsbook/";
+$db_name = "devsbook";
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "123456";
+
+$pdo = new PDO("mysql:dbname={$db_name};host={$db_host};", $db_user, $db_pass);
